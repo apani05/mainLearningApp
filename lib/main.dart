467316500d692e58023b43ef56e279b52f188172
unknown <chadalavada.harika27@gmail.com>
2023-11-30 12:45:22 -0800

    toChildHarika

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,30 @@
-import 'package:firebase_core/firebase_core.dart';
+import 'package:bfootlearn/route/routes.dart';
 import 'package:flutter/material.dart';
-import 'auth/auth.dart';
-import 'auth/login_or_register.dart';
-import 'firebase_options.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+import 'Home/views/home_page.dart';
+
+void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const AuthPage(),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      onGenerateRoute: RouteGenerator.generateRoute,
+      home:  HomePage(),
     );
   }
 }
+
+
+
+
